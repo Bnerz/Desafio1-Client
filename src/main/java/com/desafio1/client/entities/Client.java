a4522c4957d4e5a8cedf8ea849c70a0d77cf9365
Abner Teixeira <abner.ba81@gmail.com>
2021-12-22 14:52:05 +0000

    Auditing Data

--- src/main/java/com/desafio1/client/entities/Client.java
+++ src/main/java/com/desafio1/client/entities/Client.java
@@ -4,17 +4,20 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_client")
-public class Client implements Serializable{
+public class Client implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -23,12 +26,16 @@ public class Client implements Serializable{
 	private Double income;
 	private Instant birthDate;
 	private Integer children;
-	
-	
-	public Client() {
-		
-	}
 
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
+
+	public Client() {
+
+	}
 
 	public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer childrem) {
 		super();
@@ -40,72 +47,76 @@ public class Client implements Serializable{
 		this.children = childrem;
 	}
 
-
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	public String getCpf() {
 		return cpf;
 	}
-
 
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
 
-
 	public Double getIncome() {
 		return income;
 	}
-
 
 	public void setIncome(Double income) {
 		this.income = income;
 	}
 
-
 	public Instant getBirthDate() {
 		return birthDate;
 	}
-
 
 	public void setBirthDate(Instant birthDate) {
 		this.birthDate = birthDate;
 	}
 
-
 	public Integer getChildren() {
 		return children;
 	}
-
 
 	public void setChildren(Integer children) {
 		this.children = children;
 	}
 
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
+	}
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(birthDate, children, cpf, id, income, name);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -120,8 +131,5 @@ public class Client implements Serializable{
 				&& Objects.equals(cpf, other.cpf) && Objects.equals(id, other.id)
 				&& Objects.equals(income, other.income) && Objects.equals(name, other.name);
 	}
-	
-	
-}
 
-	
+}
--- src/main/java/com/desafio1/client/services/ClientService.java
+++ src/main/java/com/desafio1/client/services/ClientService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.desafio1.client.dto.ClientDTO;
 import com.desafio1.client.entities.Client;
 import com.desafio1.client.repositories.ClientRepository;
+import com.desafio1.client.services.exceptions.DatabaseException;
 import com.desafio1.client.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -63,5 +66,17 @@ public class ClientService {
 		catch(EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
 		}		
+	}
+
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity Violation");
+		}
 	}
 }
